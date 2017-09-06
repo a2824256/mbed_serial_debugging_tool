474ec8ca0e35d818a42697a7ccab6b5109a0534b
Alex Leung <lygg725@163.com>
2017-09-06 21:46:01 +0800

    更新连接

--- mbed_connect/mbed_connect/MainWindow.xaml.cs
+++ mbed_connect/mbed_connect/MainWindow.xaml.cs
@@ -28,32 +28,11 @@ namespace mbed_connect
         private bool ComPortIsOpen = false;//COM口开启状态字，在打开/关闭串口中使用，这里没有使用自带的ComPort.IsOpen，因为在串口突然丢失的时候，ComPort.IsOpen会自动false，逻辑混乱  
         private bool Listening = false;//用于检测是否没有执行完invoke相关操作，仅在单线程收发使用，但是在公共代码区有相关设置，所以未用#define隔离  
         private bool WaitClose = false;//invoke里判断是否正在关闭串口是否正在关闭串口，执行Application.DoEvents，并阻止再次invoke ,解决关闭串口时，程序假死，具体参见http://news.ccidnet.com/art/32859/20100524/2067861_4.html 仅在单线程收发使用，但是在公共代码区有相关设置，所以未用#define隔离  
-        IList<customer> comList = new List<customer>();//可用串口集合  
         DispatcherTimer autoSendTick = new DispatcherTimer();//定时发送  
-#if MULTITHREAD  
-        private static bool Sending = false;//正在发送数据状态字  
-        private static Thread _ComSend;//发送数据线程  
-        Queue recQueue = new Queue();//接收数据过程中，接收数据线程与数据处理线程直接传递的队列，先进先出  
-        private  SendSetStr SendSet = new SendSetStr();//发送数据线程传递参数的结构体  
-        private  struct SendSetStr//发送数据线程传递参数的结构体格式  
-        {  
-            public string SendSetData;//发送的数据  
-            public bool? SendSetMode;//发送模式  
-        }  
-#endif  
-        public class customer//各下拉控件访问接口  
-        {
-
-            public string com { get; set; }//可用串口  
-            public string com1 { get; set; }//可用串口  
-            public string BaudRate { get; set; }//波特率  
-            public string Parity { get; set; }//校验位  
-            public string ParityValue { get; set; }//校验位对应值  
-            public string Dbits { get; set; }//数据位  
-            public string Sbits { get; set; }//停止位  
+        private String PortName = "";
+        private bool PortSwitch = false;
 
 
-        }
 
         public MainWindow()
         {
@@ -62,10 +41,17 @@ namespace mbed_connect
 
         private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            String ComName = "";
-            ComName = Cb.SelectedValue.ToString();
-            ContentBox.Text = ContentBox.Text + "已自动选择" + ComName + "...\n";
-            ContentBox.Text = ContentBox.Text + "Waiting connect...\n";
+            try
+            {
+                this.PortName = Cb.SelectedValue.ToString();
+                ContentBox.Text = ContentBox.Text + "已自动选择" + this.PortName + "...\n";
+                ContentBox.Text = ContentBox.Text + "Waiting connect...\n";
+            }
+            catch (Exception ex)
+            {
+                this.PrintError(ex);
+                Cb.Items.Clear();
+            }
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
@@ -87,18 +73,69 @@ namespace mbed_connect
             }
             else//未检测到串口  
             {
+                Cb.Items.Clear();
+                this.PortName = "";
                 ContentBox.Text = ContentBox.Text + "无可用串口...\n";
             }
         }
 
         private void Button_Connect(object sender, RoutedEventArgs e)
         {
+            if (!PortSwitch)
+            {
+                try
+                {
+                    this.ComPort.PortName = this.PortName;
+                    this.ComPort.BaudRate = 9600;
+                    this.ComPort.Open();
+                    this.PortSwitch = true;
+                    ConnectButton.DataContext = "1";
+                    ConnectButton.Content = "Disconnect";
+                    ContentBox.Text = ContentBox.Text + this.PortName + "已连接...\n";
+                }
+                catch (Exception ex)
+                {
+                    this.PortSwitch = false;
+                    this.PrintError(ex);
+                    ContentBox.Text = ContentBox.Text + "串口打开失败\n";
+                }
+            }else if(PortSwitch)
+            {
+                try
+                {
+                    this.ComPort.Close();
+                    ConnectButton.DataContext = "0";
+                    ConnectButton.Content = "Connect";
+                    this.PortSwitch = false;
+                    ContentBox.Text = ContentBox.Text + "串口已断开...\n";
+                }
+                catch (Exception ex)
+                {
+                    this.PrintError(ex);
+                    ConnectButton.Content = "Connect";
+                    this.PortSwitch = false;
+                    ContentBox.Text = ContentBox.Text + "串口关闭失败\n";
+                }
+            }
+            
+           
+            
+
 
         }
 
         private void Clean_Content(object sender, RoutedEventArgs e)
         {
             ContentBox.Text = "";
+        }
+
+        private void PrintError(Exception ex)
+        {
+            Console.WriteLine(ex);
+            ContentBox.Text = ContentBox.Text + "-----------发生异常----------\n";
+            ContentBox.Text = ContentBox.Text + ex + "\n\n";
+            ContentBox.Text = ContentBox.Text + "请等待作者修复\n";
+            ContentBox.Text = ContentBox.Text + "------------------------------\n";
         }
     }
 }
