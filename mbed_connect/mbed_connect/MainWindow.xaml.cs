1e62346ec728738a8366211c3485a82800e91bf8
Alex Leung <lygg725@163.com>
2017-09-30 20:32:27 +0800

    更新代码

--- mbed_connect/mbed_connect/MainWindow.xaml.cs
+++ mbed_connect/mbed_connect/MainWindow.xaml.cs
@@ -11,7 +11,7 @@ using System.Windows.Shapes;
 
 namespace mbed_connect
 {
-    /// <summary>
+    /// <summary> 
     /// MainWindow.xaml 的交互逻辑
     /// </summary>
     public partial class MainWindow : Window
